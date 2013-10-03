eaacf7a99924d8a9ab02979fa4421922bfc554c1
Teesy <tonyl7126@gmail.com>
2013-10-03 15:35:29 -0700

    add fabfile

---
+++ fabfile.py
@@ -0,0 +1,28 @@
+from fabric.api import *
+from fabric.contrib import files
+from fabric.operations import put
+def setup_mo_repo():
+    run("mkdir -p git_key")
+    with cd("/home/ubuntu/git_key"):
+        if not files.exists("./rtb_key"):
+            with lcd("%s/conf/git_keys" % os.getcwd()):
+                put("rtb_key", "", use_sudo = True)
+    run("chmod 0600 rtb_key")
+    try:
+        run("")  ##too sick
+    except:
+        pass
+    sudo("pip install virtualenv")
+    if not files.exists("momo"):
+        run("virtualenv -p /usr/bin/python2.7 momo")
+    with cd("momo"):
+        run("source bin/activate")
+        run("eval `ssh-agent` && ssh-add /home/ubuntu/rtb_key && git clone git@github.com:tony7126/momonitor.git")
+        with cd("momonitor"):
+            run("pip install -r requirements.txt")
+
+@hosts(['mongo-backup.lyfemobile.net'])
+def deploy_mo():
+    env.user = "ubuntu"
+    with cd("momo/momonitor"):
+        run("git pull origin master")    
